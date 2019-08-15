34e18302a38ced63b6bcd1a1f4a4ef4d3ec0a065
Victoriaspurlieu <burberry0812sh@gmail.com>
2019-08-14 21:26:03 -0700

    Created React App

---
+++ client/build/precache-manifest.902924ea08cadbdf558d50b8da9c41c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea58435a877ba737a740",
+    "url": "/static/css/main.c059b0ad.chunk.css"
+  },
+  {
+    "revision": "ea58435a877ba737a740",
+    "url": "/static/js/main.d7f8854f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "0ac69a7e34e0f931b33e1ccd8a740ce3",
+    "url": "/index.html"
+  }
+];
