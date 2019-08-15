c66d4f74d45a49324165ed6ab00ba697ef2a6c6a
Victoriaspurlieu <burberry0812sh@gmail.com>
2019-08-14 21:34:00 -0700

    created mongodb connection

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
@@ -10,15 +11,12 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+const mongoose = require("mongoose");
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/googlebooks";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-// Define API routes here
-
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
+require("./routes/api-routes")(app);
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
